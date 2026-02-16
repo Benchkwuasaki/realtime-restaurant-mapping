68e311d46faebf2cdff2abdf6fef6886a9f2a721
Yobe <s.amoy.earlfrancisphilip@cmu.edu.ph>
2026-02-16 15:44:52 +0800

    Updated Theme

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -21,6 +22,7 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    
     esbuild: {
         jsx: 'automatic',
     },
