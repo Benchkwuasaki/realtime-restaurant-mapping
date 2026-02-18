d86bea7d78507d8c57842008f9b4ae4e3d961763
Yobe <earlfrancisphilipamoy@gmail.com>
2026-02-18 16:36:34 +0800

    Document Tracker Partial Design
    Added Table
    Placeholders
    Pagination
    Search Bar

--- resources/js/pages/DocumentTracking/Index.tsx
+++ resources/js/pages/DocumentTracking/Index.tsx
@@ -2,7 +2,25 @@ import { Head } from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
-import { dashboard } from '@/routes';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { MoreVerticalIcon } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,12 +29,147 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Separator } from '@radix-ui/react-dropdown-menu';
+import { FieldLabel } from '@/components/ui/field';
+
 export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Document Tracking" />
             <div>
-                
+                <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        </div>
+                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        </div>
+                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        </div>
+                    </div>
+
+                    <div className='flex justify-between'>
+                        <Input placeholder="Search documents..." className="w-100" />
+                        <Button>Create Request</Button>
+                    </div>
+
+                    <div className="rounded-2xl shadow-xl border p-2">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Title</TableHead>
+                                    <TableHead>Requesting Office</TableHead>
+                                    <TableHead>Forwarding Office</TableHead>
+                                    <TableHead>Current Holder</TableHead>
+                                    <TableHead>Step Status</TableHead>
+                                    <TableHead>Action</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className="font-medium">Budget Proposal</TableCell>
+                                    <TableCell>HR Office</TableCell>
+                                    <TableCell>Legal Office</TableCell>
+                                    <TableCell>Manager Office</TableCell>
+                                    <TableCell>Received</TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="ghost" size="icon" className="size-8">
+                                                <MoreVerticalIcon />
+                                                <span className="sr-only">Open menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="center">
+                                            <DropdownMenuItem>View</DropdownMenuItem>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem variant="destructive">
+                                                Delete
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="font-medium">Budget Proposal</TableCell>
+                                    <TableCell>HR Office</TableCell>
+                                    <TableCell>Legal Office</TableCell>
+                                    <TableCell>Manager Office</TableCell>
+                                    <TableCell>Received</TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="ghost" size="icon" className="size-8">
+                                                <MoreVerticalIcon />
+                                                <span className="sr-only">Open menu</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="center">
+                                            <DropdownMenuItem>View</DropdownMenuItem>
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem variant="destructive">
+                                                Delete
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </div>
+
+
+                    <Separator className="my-5 border-1" />
+
+                    <div className="flex items-center justify-between gap-4">
+                        <div className="flex items-center gap-2 w-fit">
+                            <FieldLabel htmlFor="select-rows-per-page">Rows per page</FieldLabel>
+                            <Select defaultValue="10">
+                                <SelectTrigger className="w-20" id="select-rows-per-page">
+                                    <SelectValue />
+                                </SelectTrigger>
+                                <SelectContent align="start">
+                                    <SelectGroup>
+                                        <SelectItem value="10">10</SelectItem>
+                                        <SelectItem value="25">25</SelectItem>
+                                        <SelectItem value="50">50</SelectItem>
+                                        <SelectItem value="100">100</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <Pagination className="mx-0 w-auto">
+                            <PaginationContent>
+                                <PaginationItem>
+                                    <PaginationPrevious href="#" />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationNext href="#" />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </div>
+                </div>
+
             </div>
         </AppLayout>
     );
