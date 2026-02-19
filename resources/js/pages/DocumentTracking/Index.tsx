45036e6ada4a2b1df35b34c852181c57e5bc6ead
Yobe <earlfrancisphilipamoy@gmail.com>
2026-02-19 15:48:19 +0800

    Added Card
    Added Card for the Document Tracking

--- resources/js/pages/DocumentTracking/Index.tsx
+++ resources/js/pages/DocumentTracking/Index.tsx
@@ -1,7 +1,9 @@
 import { Head } from '@inertiajs/react';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
+import TablePage from '@/components/DocumentTracking/page';
+import { Card, CardDescription, CardTitle } from '@/components/ui/card';
+import { File, Inbox, Check } from "lucide-react";
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -9,29 +11,66 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: 'document_tracking',
     },
 ];
-import TablePage from '@/components/DocumentTracking/DocumentTable/page';
-
 
 export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Document Tracking" />
             <div>
-                <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
-                        <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                        </div>
+                <div className="flex h-full flex-1 flex-col overflow-x-auto rounded-xl p-4">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-3 ">
+                        <Card className="p-4 border border-gray-200">
+                            <div className="flex items-center gap-4">
+                                <File className="size-10 shrink-0" />
+                                <div className="flex flex-col">
+                                    <CardTitle className="text-2xl font-mono font-thin">
+                                        Total Requests
+                                    </CardTitle>
+                                    <CardTitle className="text-3xl font-mono">
+                                        4
+                                    </CardTitle>
+                                    <CardDescription className="text-lg font-mono font-thin">
+                                        Incoming and outgoing requests
+                                    </CardDescription>
+                                </div>
+                            </div>
+                        </Card>
+                        <Card className="p-4 border border-gray-200">
+                            <div className="flex items-center gap-4">
+                                <Check className="size-10 shrink-0" />
+                                <div className="flex flex-col">
+                                    <CardTitle className="text-2xl font-mono font-thin">
+                                        Total Done Requests
+                                    </CardTitle>
+                                    <CardTitle className="text-3xl font-mono">
+                                        1
+                                    </CardTitle>
+                                    <CardDescription className="text-lg font-mono font-thin">
+                                        Checked and reviewed requests
+                                    </CardDescription>
+                                </div>
+                            </div>
+                        </Card>
+                        <Card className="p-4 border border-gray-200">
+                            <div className="flex items-center gap-4">
+                                <Inbox className="size-10 shrink-0" />
+                                <div className="flex flex-col">
+                                    <CardTitle className="text-2xl font-mono font-thin">
+                                        Incoming Requests
+                                    </CardTitle>
+                                    <CardTitle className="text-3xl font-mono">
+                                        3
+                                    </CardTitle>
+                                    <CardDescription className="text-lg font-mono font-thin">
+                                        Requests to process
+                                    </CardDescription>
+                                </div>
+                            </div>
+                        </Card>
                     </div>
                     <TablePage />
                 </div>
             </div>
-        </AppLayout>
+        </AppLayout >
     );
 }
