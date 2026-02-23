f783ca199d530a206d55a467ca6d8baa82b3e4ad
Yobe <earlfrancisphilipamoy@gmail.com>
2026-02-23 11:40:14 +0800

    Update page.tsx

--- resources/js/components/Employeee/page.tsx
+++ resources/js/components/Employeee/page.tsx
@@ -19,17 +19,7 @@ export default function TaskPage() {
   const { tasks } = usePage<Props>().props
 
   return (
-    <div className="hidden h-full flex-1 flex-col gap-8 p-8 md:flex">
-      <div className="flex items-center justify-between gap-2">
-        <div className="flex flex-col gap-1">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            Welcome back!
-          </h2>
-          <p className="text-muted-foreground">
-            Here&apos;s a list of your tasks for this month.
-          </p>
-        </div>
-      </div>
+    <div className="hidden h-full flex-1 flex-col gap-8 p-4 md:flex">
       <DataTable data={tasks} columns={columns} />
     </div>
   )
