0e8025ae9b354a0ed4df85b98b8783e380cfe459
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2026-03-03 09:39:33 +0800

    setup camera, face api call working, detection and db write not yet

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -111,6 +111,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         show: hasRole('ogm') || hasRole('hr_admin') || hasRole('super_admin'),
         items: [
           {
+            title: "Attendance Logs",
+            url: route('attendance-logs.index'),
+          },
+          {
             title: "Whereabout Slip",
             url: route('whereabout-slip.index'),
           },
